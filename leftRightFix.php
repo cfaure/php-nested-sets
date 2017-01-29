667520ee16d4309601a8b611a88584688f1fd913
cfaure <cfaure@users.noreply.github.com>
2017-01-29 16:50:41 +0100

    Update leftRightFix.php
    Class packaging. Reading to run on "entity" table.

--- leftRightFix.php
+++ leftRightFix.php
@@ -1,68 +1,124 @@
 <?php
-
-/*$fullPaths = [
-    'A'  => 'A',
-    'B'  => 'A / B',
-    'C1' => 'A / B / C',
-    'D'  => 'A / B / D',
-    'C2' => 'A / C',
-    'E'  => 'E',
-    'F'  => 'E / F',
-    'G'  => 'G'
-];*/
-
-$fullPaths = [];
-$dbh  = new \PDO('mysql:host=localhost;dbname=veolia', 'root', '') or die('Cnx impossible');
-$stmt = $dbh->prepare('select entity_id, full_label from entity where client_id = 103 order by parent_id ASC, full_label ASC');
-$stmt->execute();
-
-while ($res = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    $fullPaths[$res['entity_id']] = $res['full_label'];
-}
-
-
-function getNextChild(array &$tree, $key, $path)
+/**
+ *
+ */
+class FixEntities
 {
-    $nodes = explode(' / ', $path, 2);
-    $node  = $nodes[0];
-    if (!isset($tree[$node])) {
-        $tree[$node] = [
-            'key'      => null,
-            'children' => []
-        ];
-    } else {
+    protected $dbh;
 
+    /**
+     * [__construct description]
+     * @param [type] $dsn  [description]
+     * @param [type] $user [description]
+     * @param [type] $pass [description]
+     */
+    public function __construct($dsn, $user, $pass)
+    {
+        try {
+            $this->dbh = new \PDO($dsn, $user, $pass);
+        } catch (Exception $e) {
+            die("Connexion bdd impossible.\n");
+        }
     }
-    if (count($nodes) > 1) {
-        getNextChild($tree[$node]['children'], $key, $nodes[1]);
-    } else {
-        // La clé correspond toujours au dernier niveau du full_label
-        $tree[$node]['key'] = $key;
-        //echo "setting up key: $key\n";
+
+    /**
+     * Construction de l'arbre en stockage récursif
+     * @param  array  $tree [description]
+     * @param  [type] $key  [description]
+     * @param  [type] $path [description]
+     * @return [type]       [description]
+     */
+    protected function getNextChild(array &$tree, $key, $path)
+    {
+        $nodes = explode(' / ', $path, 2);
+        $node  = $nodes[0];
+        if (!isset($tree[$node])) {
+            $tree[$node] = [
+                'key'      => null,
+                'children' => []
+            ];
+        }
+        if (count($nodes) > 1) {
+            $this->getNextChild($tree[$node]['children'], $key, $nodes[1]);
+        } else {
+            // La clé correspond toujours au dernier niveau du full_label
+            $tree[$node]['key'] = $key;
+        }
+    }
+
+    /**
+     * Mise à jour des lft/rgt à partir du stockage récursif
+     * @param  array  $node [description]
+     * @param  [type] $lft  [description]
+     * @param  [type] $stmt [description]
+     * @return [type]       [description]
+     */
+    protected function updateNextNode(array &$node, &$lft, PDOStatement $stmt)
+    {
+        $node['lft'] = $lft;
+        foreach ($node['children'] as &$child) {
+            $lft = $lft + 1;
+            $this->updateNextNode($child, $lft, $stmt);
+        }
+        $lft         = $lft + 1;
+        $node['rgt'] = $lft;
+        $stmt->execute([
+            'lft' => $node['lft'],
+            'rgt' => $node['rgt'],
+            'entityId'  => $node['key']
+        ]);
+    }
+
+    /**
+     * Run fix entity script.
+     * @param  int $clientId  [description]
+     * @param  int $updatedBy [description]
+     * @param  int $parentId  [description]
+     * @return void
+     */
+    public function run($clientId, $updatedBy, $parentId = null)
+    {
+        $parentIdFilter = $parentId !== null
+            ? ' AND parent_id = '.(int)$parentId
+            : '';
+        $stmt = $this->dbh->prepare('
+            SELECT entity_id, full_label
+            FROM entity
+            WHERE client_id = '.(int)$clientId.'
+            '.$parentIdFilter.'
+            ORDER BY parent_id ASC, full_label ASC, lft ASC
+        ');
+        $stmt->execute();
+        $revFullPaths = [];
+        $fullPaths    = [];
+        while ($res = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            if (array_key_exists($res['full_label'], $revFullPaths)) {
+                echo "ERROR (should stop) duplicate full_label: ".$res['full_label']."\n";
+                continue;
+            }
+            $revFullPaths[$res['full_label']] = $res['entity_id'];
+            $fullPaths[$res['entity_id']]     = $res['full_label'];
+        }
+
+        $rootTrees = [];
+        foreach ($fullPaths as $key => $fullPath) {
+            $this->getNextChild($rootTrees, $key, $fullPath);
+        }
+
+        $updateStmt = $this->dbh->prepare('
+            UPDATE entity
+            SET lft = :lft, rgt = :rgt, updated_by = '.(int)$updatedBy.', updated_at = NOW()
+            WHERE
+                client_id = '.(int)$clientId.'
+                AND entity_id = :entityId
+                AND (lft != :lft OR rgt != :rgt)
+        ');
+        foreach ($rootTrees as &$rootTree) {
+            $lft = 1;
+            $this->updateNextNode($rootTree, $lft, $updateStmt);
+        }
     }
 }
 
-function updateNextNode(array &$node, &$lft)
-{
-    $node['lft'] = $lft;
-    foreach ($node['children'] as &$child) {
-        $lft = $lft + 1;
-        updateNextNode($child, $lft);
-    }
-    $lft         = $lft + 1;
-    $node['rgt'] = $lft;
-}
-
-// construction de l'arbre, stockage dans $tree
-$rootTrees = [];
-foreach ($fullPaths as $key => $fullPath) {
-    //echo "cur path: $fullPath\n";
-    getNextChild($rootTrees, $key, $fullPath);
-    //echo "---\n";
-}
-
-// pour tous les "roots", mise à jour des lft/rgt
-foreach ($rootTrees as &$rootTree) {
-    $lft = 1;
-    updateNextNode($rootTree, $lft);
-}
+$fix = new FixEntities('mysql:host=localhost;dbname=entity', 'root', '');
+$fix->run(103, 7719, 178546);
